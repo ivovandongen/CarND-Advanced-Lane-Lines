106e7657ba25fb6d8fd0d599774d82b34a5ef6f0
Ivo van Dongen <info@ivovandongen.nl>
2018-06-13 14:30:25 +0300

    Save camera calibration to pickle file to make iteration faster

--- camera_calibration.py
+++ camera_calibration.py
@@ -1,6 +1,9 @@
 import numpy as np
 import cv2
-import glob
+from glob import glob
+import pickle
+from os import path
+import matplotlib.pyplot as plt
 
 
 class CameraCalibration:
@@ -34,35 +37,71 @@ class CameraCalibration:
             if ret:
                 objpoints.append(objp)
                 imgpoints.append(corners)
+            else:
+                print("Checkerboard not found, skipping", fname)
 
         ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_shape, None, None)
         if ret:
             self.matrix = mtx
             self.distortion = dist
 
+
     def undistort(self, img):
         assert (self.matrix is not None)
         return cv2.undistort(img, self.matrix, self.distortion, None, self.matrix)
+
+    def save_to_file(self, file):
+        pickle.dump(self, file)
+
+
+def load_from(file):
+    return pickle.load(file)
+
+
+def default_camera_calibration(reset=False):
+    file = 'camera_calibration.p'
+    if not reset and path.exists(file):
+        print("Return saved camera calibration")
+        with open(file, 'rb') as f:
+            return load_from(f)
+    else:
+        print("Calibrating camera")
+        calibration = CameraCalibration()
+        calibration.calibrate(glob('camera_cal/calibration*'), checkerboard_shape=(9, 6), img_shape=(1280, 720))
+
+        with open(file, 'wb') as f:
+            print("Saving calibration for next run")
+            calibration.save_to_file(f)
+
+        return calibration
 
 
 def main():
     # Make a list of calibration images
     print("Calculating distortion matrix")
-    images = glob.glob('camera_cal/calibration*.jpg')
-    calibration = CameraCalibration()
-    calibration.calibrate(images, checkerboard_shape=(9, 6), img_shape=(1280, 720))
+    calibration = default_camera_calibration()
 
+    images = glob('camera_cal/calibration*.jpg')
     print("Showing undistorted images")
     for fname in images:
+        print("Processing", fname)
         img = cv2.imread(fname)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         ret, corners = cv2.findChessboardCorners(gray, (9, 6), None)
-        if ret:
-            img = cv2.drawChessboardCorners(img, (9, 6), corners, ret)
+        checker_board = cv2.drawChessboardCorners(img, (9, 6), corners, ret) if ret else img
         undist = calibration.undistort(img)
-        cv2.imshow('img', undist)
-        cv2.waitKey(500)
-    cv2.destroyAllWindows()
+
+        f, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(32, 9))
+        ax1.set_title('Original', fontsize=20)
+        ax1.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+
+        ax2.set_title('Checkerboard' + ('' if ret else ' could not be applied'), fontsize=20)
+        ax2.imshow(checker_board, cmap='gray')
+
+        ax3.set_title('Undistorted', fontsize=20)
+        ax3.imshow(undist)
+
+        plt.savefig('output_images/undistorted_' + fname.split('/')[-1])
 
 
 if __name__ == '__main__':
